dd37ce0ab7ef893d0271acaa70539f9afec7eafd
Alexis Demetriou <alex.demetriouad@gmail.com>
2024-03-27 22:57:51 +0200

    Added score system, timer, and 2D background for the level
    -Implemented a score system that tracks the player's progress in the game. The score increases over time, providing a measure of the player's performance.
    -Additionally, integrated a timer to display the time the player has been playing, formatted as MM:SS.
    -Added a 2D background to enhance the visual appeal of the level.

--- Source/HyperCube/EndlessGameModeBase.cpp
+++ Source/HyperCube/EndlessGameModeBase.cpp
@@ -7,6 +7,11 @@ void AEndlessGameModeBase::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Default"));
+
+	// Start the timer for score increase
+	GetWorldTimerManager().SetTimer(ScoreIncreaseTimerHandle, this, &AEndlessGameModeBase::IncreaseScore, ScoreIncreaseInterval, true);
+
+	GetWorldTimerManager().SetTimer(PlaytimeUpdateTimerHandle, this, &AEndlessGameModeBase::UpdatePlaytime, TimerUpdate, true);
 }
 
 void AEndlessGameModeBase::PointScored()
@@ -18,4 +23,40 @@ void AEndlessGameModeBase::PointScored()
 int AEndlessGameModeBase::GetPointScored()
 {
 	return pointsScored;
+}
+
+void AEndlessGameModeBase::IncreaseScore()
+{
+	score++; // Increase the score
+}
+
+FString AEndlessGameModeBase::GetScore()
+{
+    // Convert the score to a string
+    FString ScoreString = FString::FromInt(score);
+
+    // Calculate the number of zeros to prepend
+    int32 ZerosToAdd = 5 - ScoreString.Len();
+
+    // Prepend zeros to the score string
+    FString FormattedScore = FString::Printf(TEXT("%0*d"), ZerosToAdd, 0) + ScoreString;
+
+    return FormattedScore;
+}
+
+void AEndlessGameModeBase::UpdatePlaytime()
+{
+	Playtime += 1.0f;
+}
+
+FString AEndlessGameModeBase::GetPlaytime()
+{
+	// Calculate minutes and seconds
+	int32 Minutes = FMath::FloorToInt(Playtime / 60.0f);
+	int32 Seconds = FMath::FloorToInt(FMath::Fmod(Playtime, 60.0f));
+
+	// Format playtime as MM:SS
+	FString FormattedPlaytime = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+
+	return FormattedPlaytime;
 }
